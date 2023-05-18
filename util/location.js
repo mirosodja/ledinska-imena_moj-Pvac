29338d5c093d32043033b94dc4fef6bdc2c26e3d
mirosodja <miro.sodja@gmail.com>
2023-05-18 13:16:12 +0200

    Changed the order of lat and lng parameters in the getLedinskoIme function for consistency with other functions. Also added error handling for fetch requests.

--- util/location.js
+++ util/location.js
@@ -24,20 +24,24 @@ export async function getAddress(lat, lng) {
     return address;
 }
 
-async function getLedinskoIme(lng, lat) {
+export async function getLedinskoIme(lat, lng) {
     const featureTypes = ['point', 'line'];
     let minDistance = Infinity;
     let nearestFeature = null;
 
     for (let featureType of featureTypes) {
         const url = `https://api.mapbox.com/v4/miro-sodja.ledinska-imena-${featureType}-tiles/tilequery/${lng},${lat}.json?radius=300&limit=1&dedupe&access_token=${MAP_BOX_TOKEN}`;
-        
+
         const response = await fetch(url);
-        
-        if (response.data.features.length > 0) {
-            const feature = response.data.features[0];
+        if (!response.ok) {
+            throw new Error('Napaka pri iskanju naslova!');
+        }
+        const data = await response.json();
+
+        if (data.features.length > 0) {
+            const feature = data.features[0];
             const distance = feature.properties.tilequery.distance;
-            
+
             if (distance < minDistance) {
                 minDistance = distance;
                 nearestFeature = feature;
